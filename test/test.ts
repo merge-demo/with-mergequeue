13ce237c6e9b62ad10d6eb211a1dba71d8901807
Matt Matheson <matt.matheson@outlook.com>
2022-06-02 13:01:29 -0700

    returns you're wrong for not Heisenberg

--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,13 @@
 import { expect } from "chai";
 import sayMyName from "../lib.ts";
-
 describe("lib", function () {
   describe("#sayMyName()", function () {
     it("should return a thumbs up for the right name", function () {
       expect(sayMyName("Heisenberg")).to.eq("You're right 👍");
     });
+
+    it("should return a thumbs down for the wrong name", function () {
+      expect(sayMyName("Walter")).to.eq("You're wrong 👎");
+    })
   });
 });
